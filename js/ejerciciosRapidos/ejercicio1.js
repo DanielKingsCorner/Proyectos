a4288ae576f3d54305e41a1a938ab4757b95ee25
Daniel RH <danielrios01bp@gmail.com>
2025-05-20 14:21:12 +0200

    Bucles JS

--- js/ejerciciosRapidos/ejercicio1.js
+++ js/ejerciciosRapidos/ejercicio1.js
@@ -2,7 +2,7 @@ console.log("Hola mundo")//lo pone al inspeccionar el documento (en la consola)
 alert("Hola mundo") //Saca una alerta al abrir el html
 
 let num = 8;
-const persona; {
+const persona ; {
     nombre = "Daniel",
     edad = 23;
     esEstudiante = true;
