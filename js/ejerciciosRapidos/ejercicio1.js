c7bade2ad34ebc51981be3f057bdeb87b767a3aa
Daniel RH <danielrios01bp@gmail.com>
2025-05-20 09:56:57 +0200

    Primeros ejercios JavaScript

---
+++ js/ejerciciosRapidos/ejercicio1.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo")//lo pone al inspeccionar el documento (en la consola)
+alert("Hola mundo") //Saca una alerta al abrir el html
+
+let num = 8;
+const persona; {
+    nombre = "Daniel",
+    edad = 23;
+    esEstudiante = true;
+}
+
+console.log(persona[esEstudiante]);
---
+++ js/main.js
@@ -0,0 +1,2 @@
+//<script src="miarchivo.js"></script> ESTO SE PONE EN EL HTML CON EL QUE QUIERAS UNIRLO
+
